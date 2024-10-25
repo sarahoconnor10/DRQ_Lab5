25908399e4e894c0a72ab11cf2ea158cc40d011f
Sarah O'Connor <sarahaoconnor10@gmail.com>
2024-10-25 10:45:30 +0000

    Added route for /api/movies

--- server.js
+++ server.js
@@ -14,6 +14,39 @@ app.get('/hello/:name/:surname', (req, res) => {
     res.send(`Hello ${name} ${surname}`);
 });
 
+app.get('/hello/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Hello ${name}`);
+});
+
+app.get('/api/movies', (req, res) => {
+    const movies = [
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://example.com/poster1.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://example.com/poster2.jpg"
+        },
+        {
+            "Title": "World War Z",
+            "Year": "2013",
+            "imdbID": "tt0816711",
+            "Type": "movie",
+            "Poster": "https://example.com/poster3.jpg"
+        }
+    ];
+    res.status(201).json({ movies });
+    res.json({ movies });
+});
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
